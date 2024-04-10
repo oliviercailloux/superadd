7c5dc43e80061211f5ed455bd226d5e0c7cce09f
Olivier Cailloux <olivier.cailloux@dauphine.fr>
2024-04-10 18:16:57 +0200

    Initial commit

---
+++ src/main/java/io/github/oliviercailloux/exercices/additioner/AddImpl.java
@@ -0,0 +1,10 @@
+package io.github.oliviercailloux.exercices.additioner;
+
+public class AddImpl implements MyAdditioner {
+
+  @Override
+  public int add(int a, int b) {
+    
+  }
+  
+}
---
+++ src/main/java/io/github/oliviercailloux/exercices/additioner/MyAdditioner.java
@@ -0,0 +1,12 @@
+package io.github.oliviercailloux.exercices.additioner;
+
+public interface MyAdditioner {
+  /**
+   * Adds two integers.
+   *
+   * @param a the first integer to add
+   * @param b the second integer to add
+   * @return the sum of the two integers
+   */
+  int add(int a, int b);
+}
